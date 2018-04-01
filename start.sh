97c892804e6b8e97f6464738e0f2bcc88c4041fd
Gilles Crettenand <gilles.crettenand@liip.ch>
2018-04-01 10:51:23 +0200

    first blood

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+BUNDLE=$(which bundle)
+RBENV=$(which rbenv)
+NPM=$(which npm)
+
+if [[ -z "$BUNDLE" ]]; then
+    echo "Please install bundler"
+    exit 1
+fi
+
+if [[ -z "$NPM" ]]; then
+    echo "Please install npm"
+    exit 1
+fi
+
+BROWSERSYNC="./node_modules/.bin/browser-sync"
+if [[ ! -x "$BROWSERSYNC" ]]; then
+    ${NPM} install
+fi
+
+COMMAND="bundle exec jekyll serve"
+if [[ -n "$RBENV" ]]; then
+    COMMAND="$RBENV exec $COMMAND"
+fi
+
+${COMMAND} &
+${BROWSERSYNC} start --files "_site/" --proxy "localhost:4000" --reload-debounce 600
